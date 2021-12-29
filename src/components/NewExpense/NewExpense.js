3a4e2e8edaba10b9f8621f2aa935470a80054ab3
DmDmKr <dmitrii.krupin@delivered.io>
2021-12-29 14:41:00 +0100

    Section 4 completed

---
+++ src/components/NewExpense/NewExpense.js
@@ -0,0 +1,20 @@
+import "./NewExpense.css";
+import ExpenseForm from "./ExpenseForm";
+
+const NewExpense = (props) => {
+  const saveExpenseDataHandler = (enteredExpenseData) => {
+    const expenseData = {
+      ...enteredExpenseData,
+      id: Math.random().toString()
+    }
+    //point to call the function within App.js
+    props.onAddExpense(expenseData)
+  }
+  return (
+    <div className="new-expense">
+      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+    </div>
+  );
+};
+
+export default NewExpense;
