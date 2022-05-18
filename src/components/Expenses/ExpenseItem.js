1908135821d3c97c4f20deb641bfe27f8dee857a
DmDmKr <dmitrii.krupin@delivered.io>
2022-05-18 09:23:50 +0200

    Click handler added

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -4,16 +4,22 @@ import Card from "../UI/Card";
 import React, { useState } from "react";
 
 const ExpenseItem = (props) => {
-  useState(props.title);
+  const [title, setTitle] = useState(props.title);
+
+  const clickHandler = () => {
+    setTitle('Updated!')
+    console.log(props.title)
+  }
 
   return (
     <li>
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
+      <button onClick={clickHandler}>Change Title</button>
       </Card>
     </li>
   );
